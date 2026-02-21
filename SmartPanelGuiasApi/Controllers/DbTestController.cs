67e465e427f80c04a54358cdc77e416d77b5a22d
ADiazDev <albjesusdiaz@gmail.com>
2026-02-21 00:16:23 -0300

    rednder db

---
+++ SmartPanelGuiasApi/Controllers/DbTestController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartPanelGuiasApi.Conexion;
+using System.Collections.Generic;
+
+namespace SmartPanelGuiasApi.Controllers
+{
+    [ApiController]
+    [Route("api/testdb")]
+    public class DbTestController : ControllerBase
+    {
+        private readonly DbConexion _db;
+
+        public DbTestController(DbConexion db)
+        {
+            _db = db;
+        }
+
+        // Endpoint para listar todos los usuarios
+        [HttpGet("usuarios")]
+        public IActionResult GetUsuarios()
+        {
+            using var conn = _db.GetSmartPanelConnection();
+            conn.Open();
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM usuario;";
+            var reader = cmd.ExecuteReader();
+            var list = new List<object>();
+            while (reader.Read())
+            {
+                list.Add(new
+                {
+                    id = reader["id_usuario"],
+                    nombre = reader["nombre"],
+                    correo = reader["correo"]
+                });
+            }
+            return Ok(list);
+        }
+
+        // Opcional: lista de privilegios
+        [HttpGet("privilegios")]
+        public IActionResult GetPrivilegios()
+        {
+            using var conn = _db.GetSmartPanelConnection();
+            conn.Open();
+            using var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM privilegio;";
+            var reader = cmd.ExecuteReader();
+            var list = new List<object>();
+            while (reader.Read())
+            {
+                list.Add(new
+                {
+                    id = reader["id_privilegio"],
+                    nombre = reader["nombre_pri"],
+                    descripcion = reader["descripcion"]
+                });
+            }
+            return Ok(list);
+        }
+    }
+}
