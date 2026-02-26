45da9d71dadc2b40f3c4d17a2158e0a4ab27af22
ADiazDev <albjesusdiaz@gmail.com>
2026-02-26 11:52:10 -0300

    Updated: Guia Model

--- SmartPanelGuiasApi/Models/Guia.cs
+++ SmartPanelGuiasApi/Models/Guia.cs
@@ -1,12 +1,23 @@
-﻿namespace SmartPanelGuiasApi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartPanelGuiasApi.Models
 {
+    [Table("guias")]
     public class Guia
     {
+        [Column("nroint")]
         public int NroInt { get; set; }
+
+        [Column("tipo")]
         public string Tipo { get; set; } = "S";
+
+        [Column("folio")]
         public int Folio { get; set; }
+
+        [Column("fecha")]
         public DateTime Fecha { get; set; }
+
+        [Column("descripcion")]
         public string Descripcion { get; set; } = "";
     }
 }
- 
